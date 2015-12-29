ac7cea853919e393e89d52fab72b3326bce4fa1d
gulci <sebastian.gulczynski.poznan@gmail.com>
2015-12-29 13:04:35 +0100

    2012-12-29 contact form

---
+++ src/TheWorldVs/ViewModels/ContactViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheWorldVs.ViewModels
+{
+    public class ContactViewModel
+    {
+        // prop + tab*2
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
